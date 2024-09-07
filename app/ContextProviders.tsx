d19c0fee296f78502be7a874f490c0293ed6f719
Bobomurod <111354555+lilFrees@users.noreply.github.com>
2024-09-07 15:09:35 +0500

    added config

--- app/ContextProviders.tsx
+++ app/ContextProviders.tsx
@@ -3,23 +3,9 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { CartProvider } from "./_context/CartContext";
 import { WishlistProvider } from "./_context/WishlistContext";
-import { UserProvider, useUserStore } from "./_hooks/userStore";
-import { getSupabaseClient } from "./_lib/supabase/client";
-import { useEffect } from "react";
+import { UserProvider } from "./_hooks/userStore";
 
 export function ContextProviders({ children }: { children: React.ReactNode }) {
-  // const { setUser } = useUserStore();
-
-  // const supabase = getSupabaseClient();
-
-  // useEffect(() => {
-  //   async function fetchUser() {
-  //     const { data } = await supabase.auth.getUser();
-  //     setUser(data.user);
-  //   }
-  //   fetchUser();
-  // }, []);
-
   return (
     <UserProvider>
       <CartProvider>
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'cdn.dummyjson.com',
-				pathname: '/**',
-				port: '',
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+      },
+      {
+        protocol: "https",
+        hostname: "https://rfxujkcyzpaqbojdznsb.supabase.co/",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
