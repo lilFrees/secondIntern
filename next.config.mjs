bd7d5d750b4ad253802817a193767d84421647a8
Bobomurod <111354555+lilFrees@users.noreply.github.com>
2024-09-07 15:19:49 +0500

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "https://rfxujkcyzpaqbojdznsb.supabase.co/",
+        hostname: "rfxujkcyzpaqbojdznsb.supabase.co",
       },
     ],
   },
